e01e543d5cca46f4fb5c8787909b6bcedad135a7
grubergen <iluxa@grubergen.ru>
2015-06-08 20:30:36 +0300

    Update base64image.java

--- base64image.java
+++ base64image.java
@@ -8,13 +8,18 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException{
-		String out;
-		BufferedImage bi = new BufferedImage(200, 150, BufferedImage.TYPE_INT_RGB);
-		Graphics g = bi.getGraphics();
-		g.setColor(new Color(0, 0, 0));
-		g.fillRect(1, 1, 198, 148);
-		//TODO: полученное изображение представить в виде jpeg, закодировать через base64 и поместить строку out
-		//JPEGImageEncoder jie = JPEGCodec.createJPEGEncoder(System.out);
-		//jie.encode(bi);
+	String out;
+	BufferedImage bi = new BufferedImage(200, 150, BufferedImage.TYPE_INT_RGB);
+	Graphics g = bi.getGraphics();
+	g.setColor(new Color(0, 0, 0));
+	g.fillRect(1, 1, 198, 148);
+	BASE64Encoder encoder = new BASE64Encoder();
+	ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	try{
+		ImageIO.write(bi, "png", baos);
+		out = "<img src=\"data:image/png;base64," + encoder.encodeBuffer(baos.toByteArray()) + "\" alt=\"У вас не поддерживаются base64 изображения!!!\" />");
+	}catch(Exception e){
+		e.printStackTrace();
+	}
     }
 }
