41e9494a8d3f7a59650faf9ffbf121b566af773f
grubergen <iluxa@grubergen.ru>
2015-06-08 18:01:41 +0300

    Create base64image.java

---
+++ base64image.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+		String out;
+		BufferedImage bi = new BufferedImage(200, 150, BufferedImage.TYPE_INT_RGB);
+		Graphics g = bi.getGraphics();
+		g.setColor(new Color(0, 0, 0));
+		g.fillRect(1, 1, 198, 148);
+
+		//TODO: полученное изображение закодировать через base64 в строку out
+		//JPEGImageEncoder jie = JPEGCodec.createJPEGEncoder(System.out);
+		//jie.encode(bi);
+    }
+}
